6171caea7ee7b534e906b1f6ce24c1279d5c2574
gihhyeon <fineblue.kh@gmail.com>
2025-08-03 17:11:00 +0900

    Initial commit

---
+++ src/main/java/com/matzip/matzip/server/MatzipServerApplication.java
@@ -0,0 +1,13 @@
+package com.matzip.matzip.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatzipServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatzipServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matzip/matzip/server/MatzipServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matzip.matzip.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatzipServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
