0a2a8b8f1434700eda017e23da213d40ecb91833
gihhyeon <fineblue.kh@gmail.com>
2025-08-18 01:18:16 +0900

    feat: add BaseEntity

---
+++ src/main/java/com/matzip/domain/user/entity/UserEntity.java
@@ -0,0 +1,11 @@
+package com.matzip.domain.user.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+@Getter
+@Entity(name = "user")
+public class UserEntity {
+
+
+}
---
+++ src/main/java/com/matzip/global/entity/BaseEntity.java
@@ -0,0 +1,46 @@
+package com.matzip.global.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    protected BaseEntity() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
