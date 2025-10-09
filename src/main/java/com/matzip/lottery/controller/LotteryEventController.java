e650a2c32d2b9112cd8d2658a852bee9324c976e
jcw1031 <jcw001031@gmail.com>
2025-10-09 16:29:19 +0900

    인증된 사용자 ID 조회 방식 변경

--- src/main/java/com/matzip/lottery/controller/LotteryEventController.java
+++ src/main/java/com/matzip/lottery/controller/LotteryEventController.java
@@ -1,11 +1,11 @@
 package com.matzip.lottery.controller;
 
 import com.matzip.common.response.ApiResponse;
+import com.matzip.common.security.UserPrincipal;
 import com.matzip.lottery.controller.request.ParticipateEventRequest;
 import com.matzip.lottery.controller.response.LotteryEventResponse;
 import com.matzip.lottery.service.LotteryEventService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,15 +24,15 @@ public class LotteryEventController {
     }
 
     @GetMapping
-    public ApiResponse<LotteryEventResponse> findEvent(@AuthenticationPrincipal UserDetails user) {
-        LotteryEventResponse data = lotteryEventService.getCurrentEvent(Long.parseLong(user.getUsername()));
+    public ApiResponse<LotteryEventResponse> findEvent(@AuthenticationPrincipal UserPrincipal user) {
+        LotteryEventResponse data = lotteryEventService.getCurrentEvent(user.getUserId());
         return ApiResponse.success(data);
     }
 
     @PostMapping("/entries")
     public ApiResponse<String> participate(@Validated @RequestBody ParticipateEventRequest request,
-                                           @AuthenticationPrincipal UserDetails user) {
-        lotteryEventService.enterLottery(request.eventId(), request.ticketsCount(), Long.parseLong(user.getUsername()));
+                                           @AuthenticationPrincipal UserPrincipal user) {
+        lotteryEventService.enterLottery(request.eventId(), request.ticketsCount(), user.getUserId());
         return ApiResponse.successWithoutData();
     }
 }
