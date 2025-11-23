79226f2eec07a6916fd8432e208277956eb4d39d
gihhyeon <fineblue.kh@gmail.com>
2025-11-23 22:41:40 +0900

    admin 패키지 .gitignore 추가

--- src/main/java/com/matzip/admin/repository/RequestReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.matzip.admin.repository;
-
-import com.matzip.admin.domain.RequestReview;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RequestReviewRepository extends JpaRepository<RequestReview, Long> {
-}
