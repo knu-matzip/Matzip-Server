003b90cecee38c5daac4d8e8f3595b2f2c1a40c0
gihhyeon <fineblue.kh@gmail.com>
2025-11-26 19:42:07 +0900

    맛집 등록 현황 목록/상세 조회 API 개발

--- src/main/java/com/matzip/admin/repository/RequestReviewRepository.java
+++ src/main/java/com/matzip/admin/repository/RequestReviewRepository.java
@@ -1,9 +1,14 @@
 package com.matzip.admin.repository;
 
 import com.matzip.admin.domain.RequestReview;
+import com.matzip.admin.domain.RequestReviewStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RequestReviewRepository extends JpaRepository<RequestReview, Long> {
+
+    Optional<RequestReview> findTopByPlaceIdAndStatusOrderByCreatedAtDesc(Long placeId, RequestReviewStatus status);
 }
